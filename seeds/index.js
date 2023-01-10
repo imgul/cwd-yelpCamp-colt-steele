1d22c33cd921df16f73fda6cd27a99f59af12ea8
Gul Zaib <bc210411296mgu@vu.edu.pk>
2023-01-10 23:31:21 +0500

    complete project upto 41 YelpCamp

---
+++ seeds/index.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Campground = require('../models/campground')
+const cities = require('../seeds/cities')
+const { places, descriptors } = require('../seeds/seedHelpers')
+
+mongoose.connect('mongodb+srv://gulzaib:gulzaib@cluster0.75u1mrm.mongodb.net/yelpCamp?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    // useCreateIndex: true, // MongoParseError: option usecreateindex is not supported
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Connection Error:'))
+db.once('open', () => {
+    console.log('Database Connected Successfully.')
+})
+
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close()
+})
+
+// app.get('/makecampground', async (req, res) => {
+//     const camp = new Campground({
+//         title: 'My Campground',
+//         description: 'This is my best campground.'
+//     })
+//     await camp.save()
+//     res.send(camp)
+// })
